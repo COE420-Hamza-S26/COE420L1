540aa3d86f2a49d680252c66f08ea36cab8bdaf9
HamzaCharif <hamzaalcharif06@gmail.com>
2026-01-23 00:08:52 +0400

    Feature B added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
+
+    public static void featureB() {
+        System.out.println("Feature B executed");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+        featureB();
     }
 }
